2833944922a5cd604fd130f15552f1458e603d35
Ismail2hussein <ihusseinmohamed629@gmail.com>
2023-06-14 14:11:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// Good Luck 💪🏾
+function makeCapital(str) {
+  let words = str.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  }
+  return words.join(" ");
+}
+console.log(makeCapital("my name is ali")); // "My Name Is Ali"
+console.log(makeCapital("fikrcamp Bootcamp")); // "Fikrcamp Bootcamp"
+console.log(makeCapital("i live In Paris")); // "I Live In Paris"
